e931f9c863c3991a48237621a4bfd7b68142148e
Mac <hung.mac@dbschenker.com>
2022-03-30 21:43:26 -0400

    analog digital bootstrap

---
+++ src/app/digital-clock/digital-clock.component.ts
@@ -0,0 +1,63 @@
+import { Component, OnInit } from '@angular/core';
+import {
+  interval,
+  Subscription
+} from 'rxjs';
+
+@Component({
+  selector: 'app-digital-clock',
+  templateUrl: './digital-clock.component.html',
+  styleUrls: ['./digital-clock.component.scss']
+})
+export class DigitalClockComponent implements OnInit {
+  isStart: boolean = false;
+  subscription = new Subscription();
+
+  daysOfweek = ['sun', 'mon', 'tues', 'wed', 'thur', 'fri', 'sat'];
+  day: any;
+  hour: string;
+  minute: string
+  second: string
+  ampm: string
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+    this.startClock();
+  }
+
+  startClock(): void {
+    console.log('start');
+    if(!this.isStart) {
+      this.subscription = interval(1000).subscribe(()=>
+        this.setDate(new Date)
+      )
+      this.isStart=true;
+    }
+
+  }
+
+  setDate(date: Date): void {
+    this.day = this.daysOfweek[date.getDay()];
+    this.hour = date.getHours() % 12  ?  '12' : date.getHours().toString();
+    this.minute = date.getMinutes() < 10 ? '0' + date.getMinutes() :  date.getMinutes().toString();
+    this.second = date.getSeconds()  < 10 ? '0' + date.getSeconds() : date.getSeconds().toString();
+    this.ampm = date.getHours() < 12 ? 'AM' : 'PM';
+  }
+
+
+  stopClock() {
+    console.log('stop');
+    if(this.isStart) {
+      this.subscription.unsubscribe();
+      this.isStart=false;
+    }
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
+
+}
